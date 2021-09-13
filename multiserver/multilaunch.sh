8c31c81ca7a4e2c94af4ec16048d86589bf4bded
Civitello <61434667+Civitello@users.noreply.github.com>
2021-09-13 10:39:32 -0600

    Update multilaunch.sh

--- multiserver/multilaunch.sh
+++ multiserver/multilaunch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo nano systemctl start mcreset01
-sudo nano systemctl start mcreset02
-sudo nano systemctl start mcreset03
-sudo nano systemctl start mcreset04
-sudo nano systemctl start mcreset05
-sudo nano systemctl start mcreset06
-sudo nano systemctl start mcreset07
-sudo nano systemctl start mcreset08
-sudo nano systemctl start mcreset09
-sudo nano systemctl start mcreset10
+sudo systemctl start mcreset01
+sudo systemctl start mcreset02
+sudo systemctl start mcreset03
+sudo systemctl start mcreset04
+sudo systemctl start mcreset05
+sudo systemctl start mcreset06
+sudo systemctl start mcreset07
+sudo systemctl start mcreset08
+sudo systemctl start mcreset09
+sudo systemctl start mcreset10
