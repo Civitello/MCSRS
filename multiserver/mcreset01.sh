755adbce2e6b248e59abda0bc51cd02b8d4876ea
Civitello <61434667+Civitello@users.noreply.github.com>
2021-09-13 09:22:34 -0600

    Update mcreset01.sh

--- multiserver/mcreset01.sh
+++ multiserver/mcreset01.sh
@@ -2,11 +2,11 @@
 MC_FOLDER="/var/minecraft/minecraft01"
 while true
 do
-service minecraft status | grep 'active (running)' > /dev/null 2>&1
+service minecraft01 status | grep 'active (running)' > /dev/null 2>&1
 
 if [ $? != 0 ]
 then
         mv "$MC_FOLDER/world" "$MC_FOLDER/old_world_$(date +%H:%M)"
-        sudo service minecraft restart > /dev/null
+        sudo service minecraft01 restart > /dev/null
 fi
 done
