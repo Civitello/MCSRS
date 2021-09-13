b225d72b167ee7a3b07424d4ee2c772aed636407
Civitello <61434667+Civitello@users.noreply.github.com>
2021-09-13 10:33:13 -0600

    Create multilaunch.sh

---
+++ multiserver/multilaunch.sh
@@ -0,0 +1,10 @@
+sudo nano systemctl start mcreset01
+sudo nano systemctl start mcreset02
+sudo nano systemctl start mcreset03
+sudo nano systemctl start mcreset04
+sudo nano systemctl start mcreset05
+sudo nano systemctl start mcreset06
+sudo nano systemctl start mcreset07
+sudo nano systemctl start mcreset08
+sudo nano systemctl start mcreset09
+sudo nano systemctl start mcreset10
