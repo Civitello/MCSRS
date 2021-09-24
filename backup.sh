9fc54dc729f160d857d5538eeb0fa925fb93720b
Civitello <61434667+Civitello@users.noreply.github.com>
2021-09-24 09:51:54 -0600

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,27 +8,21 @@ sleep 300
 #initiate minecraft server save to ramdisk
 mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
 #copy from ramdisk to backup
-cp -R /var/minecraft/minecraft/world /var/minecraft/backup1
+rsync -r /var/minecraft/minecraft/world /var/minecraft/backup1
 #update latest.txt
 echo "The latest backup is /backup1" > latest.txt
-#delete out of date backup
-rm -r /var/minecraft/backup2
 sleep 300
 #initiate minecraft server save to ramdisk
 mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
 #copy from ramdisk to backup
-cp -R /var/minecraft/minecraft/world /var/minecraft/backup2
+rsync -r /var/minecraft/minecraft/world /var/minecraft/backup2
 #update latest.txt
 echo "The latest backup is /backup2" > latest.txt
-#delete out of date backup
-rm -r /var/minecraft/backup3
 sleep 300
 #initiate minecraft server save to ramdisk
 mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
 #copy from ramdisk to backup
-cp -R /var/minecraft/minecraft/world /var/minecraft/backup3
+rsync -r /var/minecraft/minecraft/world /var/minecraft/backup3
 #update latest.txt
 echo "The latest backup is /backup3" > latest.txt
-#delete out of date backup
-rm -r /var/minecraft/backup1
 done
