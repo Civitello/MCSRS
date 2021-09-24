8d171ab1084baae2ea11684f7263940a2d27ca90
Civitello <61434667+Civitello@users.noreply.github.com>
2021-09-24 09:23:52 -0600

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
+#turn off autosave to prevent conflicts.
+mcrcon -H 127.0.0.1 -P 25575 -p strong-password save-off
 #backup loop, should switch to rsync
 while true
 do
-sleep 240
-cp -R /var/minecraft/minecraft /var/minecraft/backup
+sleep 300
+#initiate minecraft server save to ramdisk
+mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
+#copy from ramdisk to backup
+cp -R /var/minecraft/minecraft /var/minecraft/backup1
+#update latest.txt
+echo "The latest backup is /backup1" > latest.txt
+#delete out of date backup
+rm -r /var/minecraft/backup2
+sleep 300
+#initiate minecraft server save to ramdisk
+mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
+#copy from ramdisk to backup
+cp -R /var/minecraft/minecraft /var/minecraft/backup2
+#update latest.txt
+echo "The latest backup is /backup2" > latest.txt
+#delete out of date backup
+rm -r /var/minecraft/backup3
+sleep 300
+#initiate minecraft server save to ramdisk
+mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
+#copy from ramdisk to backup
+cp -R /var/minecraft/minecraft /var/minecraft/backup3
+#update latest.txt
+echo "The latest backup is /backup3" > latest.txt
+#delete out of date backup
+rm -r /var/minecraft/backup1
 done
