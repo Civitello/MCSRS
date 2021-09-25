0eff8dcba22fd375f2e164fc68ad787aaf99fc89
Civitello <61434667+Civitello@users.noreply.github.com>
2021-09-25 09:47:42 -0600

    Update send.sh

--- interserver/send.sh
+++ interserver/send.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #ask for speedrun server ip address
-What is the private IP address of the speedrun server? (xxx.xxx.xxx.xxx)
+echo What is the private IP address of the speedrun server? (xxx.xxx.xxx.xxx)
 read varaddress
+#check if address is valid by contacting server
 $varaddress
+#If address invalid throw error.
+#stop and save server
+mcrcon -H 127.0.0.1 -P 25575 -p strong-password stop
+#send world folder
