b4d456cc9c418a07dadc5edc2914093ccc1123cb
Civitello <61434667+Civitello@users.noreply.github.com>
2021-09-26 11:44:48 -0600

    Update send.sh

--- interserver/send.sh
+++ interserver/send.sh
@@ -4,7 +4,14 @@ echo What is the private IP address of the speedrun server? (xxx.xxx.xxx.xxx)
 read varaddress
 #check if address is valid by contacting server
 $varaddress
-#If address invalid throw error.
+#If address invalid throw error and go back to line 3
+#prevent server from restarting on stop
+sudo systemctl stop mcreset
 #stop and save server
 mcrcon -H 127.0.0.1 -P 25575 -p strong-password stop
 #send world folder
+scp -o StrictHostKeyChecking=no /var/minecraft/minecraft/world/. ubuntu@$varaddress:/media/ramdisk/minecraft/world/.
+#start server
+ssh systemctl start minecraft
+#announce success
+ssh -o StrictHostKeyChecking=no "sudo systemctl start minecraft"
