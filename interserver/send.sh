4641843628734236ccf547f4d63f69c354b914a0
Civitello <61434667+Civitello@users.noreply.github.com>
2021-09-29 14:32:55 -0600

    Update send.sh

--- interserver/send.sh
+++ interserver/send.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#ask for speedrun server ip address
-echo What is the private IP address of the speedrun server? (xxx.xxx.xxx.xxx)
-read varaddress
-#check if address is valid by contacting server
-$varaddress
-#If address invalid throw error and go back to line 3
+#loop until address variable is filled
+while [ -z "$address" ]
+do
+#make the address variable: search latest.log for line containing the word “send” | isolate ip address from line
+address="$(grep -o 'send .*' latest.log | cut -f2- -d" " )"
+done
 #prevent server from restarting on stop
 sudo systemctl stop mcreset
 #stop and save server
 mcrcon -H 127.0.0.1 -P 25575 -p strong-password stop
 #send world folder
-scp -o StrictHostKeyChecking=no /var/minecraft/minecraft/world/. ubuntu@$varaddress:/media/ramdisk/minecraft/world/.
+scp -o StrictHostKeyChecking=no /var/minecraft/minecraft/world/. ubuntu@$address:/media/ramdisk/minecraft/world/.
 #start server
-ssh -o StrictHostKeyChecking=no -t "sudo systemctl start minecraft"
-#announce success
+ssh -o StrictHostKeyChecking=no -t ubuntu@$address "sudo systemctl start minecraft"
