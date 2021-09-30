269e4f5293aaa26985da200439e457855cfd734e
Civitello <61434667+Civitello@users.noreply.github.com>
2021-09-30 13:03:08 -0600

    Update send.sh

--- interserver/send.sh
+++ interserver/send.sh
@@ -8,8 +8,12 @@ done
 #prevent server from restarting on stop
 sudo systemctl stop mcreset
 #stop and save server
-mcrcon -H 127.0.0.1 -P 25575 -p strong-password stop
+mcrcon -H 127.0.0.1 -P 25575 -p allblack stop
+#move world folder to sendable location
+sudo mv /var/minecraft/minecraft/world/ /home/ubuntu/world/
 #send world folder
-scp -o StrictHostKeyChecking=no /var/minecraft/minecraft/world/. ubuntu@$address:/media/ramdisk/minecraft/world/.
+scp -r -o StrictHostKeyChecking=no /home/ubuntu/world ubuntu@$address:/home/ubuntu/world
+#move world folder to minecraft folder
+ssh -o StrictHostKeyChecking=no -t ubuntu@$address "sudo mv  /home/ubuntu/world/ /var/minecraft/minecraft/world/"
 #start server
 ssh -o StrictHostKeyChecking=no -t ubuntu@$address "sudo systemctl start minecraft"
