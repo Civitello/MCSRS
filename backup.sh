e8bc3f4a11063b5c4421f93386e4d844f83c3ecc
Civitello <61434667+Civitello@users.noreply.github.com>
2021-10-15 17:21:37 -0600

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,21 +8,21 @@ sleep 300
 #initiate minecraft server save to ramdisk
 mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
 #copy from ramdisk to backup
-rsync -r /var/minecraft/minecraft/world /var/minecraft/backup1
+rsync -r /var/minecraft/Tracker1.16/minecraft/world /var/minecraft/Tracker1.16/backup1
 #update latest.txt
 echo "The latest backup is /backup1" > latest.txt
 sleep 300
 #initiate minecraft server save to ramdisk
 mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
 #copy from ramdisk to backup
-rsync -r /var/minecraft/minecraft/world /var/minecraft/backup2
+rsync -r /var/minecraft/Tracker1.16/minecraft/world /var/minecraft/Tracker1.16/backup2
 #update latest.txt
 echo "The latest backup is /backup2" > latest.txt
 sleep 300
 #initiate minecraft server save to ramdisk
 mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
 #copy from ramdisk to backup
-rsync -r /var/minecraft/minecraft/world /var/minecraft/backup3
+rsync -r /var/minecraft/Tracker1.16/minecraft/world /var/minecraft/Tracker1.16/backup3
 #update latest.txt
 echo "The latest backup is /backup3" > latest.txt
 done
