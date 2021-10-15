4b1a4bbb0188b9122d7e3bf99b1a08910d44411a
Civitello <61434667+Civitello@users.noreply.github.com>
2021-10-15 17:07:06 -0600

    Update ramdisk.sh

--- ramdisk.sh
+++ ramdisk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Create and mount ramdisk to existing minecraft directory.
-mount -t tmpfs -o size=24000M tmpfs /var/minecraft/minecraft
+mount -t tmpfs -o size=16000M tmpfs /var/minecraft/minecraft
 #Copy the minecraft installation to the ramdisk.
 cp -R /var/minecraft/unused/. /var/minecraft/minecraft
 #Start minecraft and resetting
