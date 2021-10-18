d13c6566acbd5b88428f2bb5dbd7aa4da896c9e5
Civitello <61434667+Civitello@users.noreply.github.com>
2021-10-18 12:58:11 -0600

    Update ramdisk.sh

--- ramdisk.sh
+++ ramdisk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Create and mount ramdisk to existing minecraft directory.
-mount -t tmpfs -o size=16000M tmpfs /var/minecraft/Tracker1.16/minecraft
+sudo mount -t tmpfs -o size=16000M tmpfs /var/minecraft/Tracker1.16/minecraft
 #Copy the minecraft installation to the ramdisk.
-cp -R /var/minecraft/Tracker1.16/unused/. /var/minecraft/Tracker1.16/minecraft
+sudo cp -R /var/minecraft/Tracker1.16/unused/. /var/minecraft/Tracker1.16/minecraft
 #Start minecraft and resetting
-systemctl start minecraft.service
+sudo systemctl start minecraft.service
 #Start backup service
-systemctl start backup.service
+sudo systemctl start backup.service
