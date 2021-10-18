0767bc8faf03da9a6eb290d1c6b2b790e4324880
Civitello <61434667+Civitello@users.noreply.github.com>
2021-10-18 12:57:21 -0600

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 #turn off autosave to prevent conflicts.
-mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-off
+mcrcon -H 127.0.0.1 -P 25575 -p password save-off
 #backup loop, should switch to rsync
 while true
 do
 sleep 300
 #initiate minecraft server save to ramdisk
-mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
+mcrcon -H 127.0.0.1 -P 25575 -p password save-all
 #copy from ramdisk to backup
-rsync -r /var/minecraft/Tracker1.16/minecraft/world /var/minecraft/Tracker1.16/backup1
+echo backing up
+sudo rsync -r /var/minecraft/Tracker1.16/minecraft/world /var/minecraft/Tracker1.16/backup1
 #update latest.txt
 echo "The latest backup is /backup1" > latest.txt
+echo backup done
 sleep 300
 #initiate minecraft server save to ramdisk
-mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
+mcrcon -H 127.0.0.1 -P 25575 -p password save-all
 #copy from ramdisk to backup
-rsync -r /var/minecraft/Tracker1.16/minecraft/world /var/minecraft/Tracker1.16/backup2
+echo backing up
+sudo rsync -r /var/minecraft/Tracker1.16/minecraft/world /var/minecraft/Tracker1.16/backup2
 #update latest.txt
 echo "The latest backup is /backup2" > latest.txt
+echo backup done
 sleep 300
 #initiate minecraft server save to ramdisk
-mcrcon -H 127.0.0.1 -P 25575 -p PASSWORD save-all
+mcrcon -H 127.0.0.1 -P 25575 -p password save-all
 #copy from ramdisk to backup
-rsync -r /var/minecraft/Tracker1.16/minecraft/world /var/minecraft/Tracker1.16/backup3
+echo backing up
+sudo rsync -r /var/minecraft/Tracker1.16/minecraft/world /var/minecraft/Tracker1.16/backup3
 #update latest.txt
 echo "The latest backup is /backup3" > latest.txt
+echo backup done
 done
